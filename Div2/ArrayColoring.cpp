40ca1f12cd1e910b0b1d2118d9afddf094f2b150
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-17 21:55:35 +0600

    declare a vector and take input of the element.

---
+++ Div2/ArrayColoring.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // vector<int> copy_arr = arr;
+    // sort(copy_arr.begin(), copy_arr.end());
+
+    // i take it key value concept.key is card value and value will be its color
+    map<int, int> color_arr;  
+    for (int i = 0; i < n; i++)
+    {
+        color_arr[copy_arr[i]] = i % 2;
+    }
+
+    bool valid = true;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (color_arr[arr[i]] == color_arr[arr[i + 1]])
+        {
+            valid = false;
+            break;
+        }
+    }
+
+    if (valid)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
