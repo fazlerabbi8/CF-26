a6242f0b99b2e7fa086f60e17a5b813b2d941320
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-18 19:05:24 +0600

    find three continuous cell also count all empty cells. call solve function in main function.

--- cp_31_sheet_problems/CoverInWater.cpp
+++ cp_31_sheet_problems/CoverInWater.cpp
@@ -1,34 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+void solve()
+{
+    int n;
+    cin >> n;
+
+    string str;
+    cin >> str;
+
+    bool continuous_three_empty_cells = false;
+    int count_total_empty_cells = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (str[i] == '.' && i + 1 < n && str[i + 1] == '.' && i + 2 < n && str[i + 2] == '.')
+        {
+            continuous_three_empty_cells = true;
+            break;
+        }
+        if (str[i] == '.')
+        {
+            count_total_empty_cells++;
+        }
+    }
+    if (continuous_three_empty_cells)
+    {
+        cout << 2 << "\n";
+    }
+    else
+    {
+        cout << count_total_empty_cells << "\n";
+    }
+}
+
+int main()
+{
     int t;
     cin >> t;
 
-    while(t--){
-        int n;
-        cin >> n;
-
-        string str;
-        cin >> str;
-
-        bool continuous_three_empty_cells = false;
-        int count_total_empty_cells = 0;
-
-        for(int i = 0; i < n; i++){
-            if(str[i] == '.' && i + 1 < n && str[i + 1] == '.' && i + 2 < n && str[i + 2] == '.'){
-                continuous_three_empty_cells = true;
-                break;
-            }
-            if(str[i] == '.'){
-                count_total_empty_cells++;
-            }
-        }
-        if(continuous_three_empty_cells){
-            cout << 2 << "\n";
-        }else{
-            cout << count_total_empty_cells << "\n";
-        }
+    while (t--)
+    {
+        solve();
     }
     return 0;
 }
