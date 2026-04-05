9eac4bb3be7c0c3c00dcf2ccb1a789d01e59603a
fazlerabbi13 <mohammad0132794@gmail.com>
2026-04-05 22:00:02 +0600

    must negate 6 out of the 7 integers (that is, multiply them by −1). Over all possible ways to negate 6 out of the 7 integers, find the maximum possible sum of a.

--- Div4/B.cpp
+++ Div4/B.cpp
@@ -1,39 +1,39 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// void solve()
-// {
-//     int n = 7;
-//     vector<int> arr(n);
+void solve()
+{
+    int n = 7;
+    vector<int> arr(n);
 
-//     for (int i = 0; i < n; i++)
-//     {
-//         cin >> arr[i];
-//     }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 
-//     sort(arr.begin(), arr.end());
-//     int ans = 0;
-//     for (int i = 0; i < n; i++)
-//     {
-//         if (i != n - 1)
-//         {
-//             arr[i] = arr[i] * -1;
-//         }
-//         ans += arr[i];
-//     }
-//     cout << ans << "\n";
-// }
+    sort(arr.begin(), arr.end());
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i != n - 1)
+        {
+            arr[i] = arr[i] * -1;
+        }
+        ans += arr[i];
+    }
+    cout << ans << "\n";
+}
 
-// int main()
-// {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(0);
-//     cout.tie(0);
-//     int t;
-//     cin >> t;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
 
-//     while (t--)
-//         solve();
+    while (t--)
+        solve();
 
-//     return 0;
-// }
+    return 0;
+}
