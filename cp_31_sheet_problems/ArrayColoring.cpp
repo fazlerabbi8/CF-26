fc622f6cf7bde3a17cb0686df6d5ee6c5767a506
fazlerabbi13 <mohammad0132794@gmail.com>
2026-05-11 19:34:30 +0600

    finding the same parity or not.

---
+++ cp_31_sheet_problems/ArrayColoring.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+
+    if(sum % 2 == 0)
+    {
+        cout << "Yes" << "\n";
+    }
+    else
+    {
+        cout << "No" << "\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        solve();
+    }
+}
