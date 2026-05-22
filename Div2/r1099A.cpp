cf512bd86a9ab7f0c99745c6d4e6efba38e136dc
fazlerabbi13 <mohammad0132794@gmail.com>
2026-05-22 14:03:49 +0600

    construct a permutation.

---
+++ Div2/r1099A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    for (int i = n * 2; i > n; i--)
+    {
+        cout << i << "\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
