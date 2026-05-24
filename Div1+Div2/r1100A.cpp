565bcaed6e592d6b64f9c95c7688ffd898f7fdb8
fazlerabbi13 <mohammad0132794@gmail.com>
2026-05-24 17:57:33 +0600

    minimum operation need for making all the element is same.

---
+++ Div1+Div2/r1100A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int maxElement = *max_element(arr.begin(), arr.end());
+    int minElement = *min_element(arr.begin(), arr.end());
+
+    cout << (maxElement - minElement + 1) / 2 << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
