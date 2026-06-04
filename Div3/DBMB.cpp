eb4e139e555de11faffc326aa06cf3b729ba2d82
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-04 21:48:47 +0600

    array satisfy the condition of sum or not.

--- Div3/DBMB.cpp
+++ Div3/DBMB.cpp
@@ -1,13 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void solve()
+{
+    int n, s, x;
+    cin >> n >> s >> x;
 
-int main(){
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i];
+    }
+
+    if (s >= sum && (sum - s) % x == 0)
+    {
+        cout << "Yes" << "\n";
+    }
+    else
+    {
+        cout << "No" << "\n";
+    }
+}
+
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
+    int t;
+    cin >> t;
 
-    
-
+    while(t--)
+    {
+        solve();
+    }
     return 0;
 }
