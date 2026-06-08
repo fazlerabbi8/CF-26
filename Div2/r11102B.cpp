4c1455dbce1484954d03d0d4a1f5d899d721d85d
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-08 13:23:30 +0600

    checking palindrome and divisibility.

---
+++ Div2/r11102B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    if(n == 10)
+    {
+        cout << -1 << "\n";
+    }
+    else if(n % 12 == 10)
+    {
+        cout << 22 << " " << n - 22 << "\n";
+    }
+    else
+    {
+        cout << n % 12 << " " << n - (n % 12) << "\n";
+    }
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
