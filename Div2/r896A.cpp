4ebaacb1e0071dbd9fc036412dfe0a70423b9fbd
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-23 20:22:53 +0600

    make all element is zero with xor operation.

---
+++ Div2/r896A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    if (n % 2 == 0)
+    {
+        cout << 2 << "\n";
+        cout << 1 << " " << n << "\n";
+        cout << 1 << " " << n << "\n";
+    }
+    else
+    {
+        cout << 4 << "\n";
+        cout << 1 << " " << n - 1 << "\n";
+        cout << 1 << " " << n - 1 << "\n";
+        cout << n - 1 << " " << n << "\n";
+        cout << n - 1 << " " << n << "\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
