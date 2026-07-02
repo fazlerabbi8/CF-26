f6ba3ab8b5eeb2ccb8f704546c6fcbe58f4dc3be
fazlerabbi13 <mohammad0132794@gmail.com>
2026-07-02 10:33:32 +0600

    maximize the value of (an - a1).

---
+++ Div2/r819A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int ans = a[n - 1] - a[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        ans = max(ans, a[i] - a[0]);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        ans = max(ans, a[n - 1] - a[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        ans = max(ans, a[i] - a[i + 1]);
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
